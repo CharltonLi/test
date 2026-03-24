672b24a11c0f12f576489ead158753b89a2397b2
CharltonLi <charlduon@gmail.com>
2026-03-23 22:06:44 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 from st_supabase_connection import SupabaseConnection
 
+st.write("1. Script started...")
+
 # Initialize connection.
 conn = st.connection("supabase",type=SupabaseConnection)
 
+st.write("3. Connection object created. Fetching data...")
+
 # Perform query.
 rows = conn.table("Product").select("*").execute()
+
+st.write("4. Data received!")
 
 # Print results.
 for row in rows.data:
