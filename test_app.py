5334992ca8331454e9fd8d30fecff224ee96c2f4
CharltonLi <charlduon@gmail.com>
2026-03-23 22:10:57 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,4 +15,5 @@ st.write("4. Data received!")
 
 # Print results.
 for row in rows.data:
-    st.write(f"Product type: {row['product_name']}")
+    name = row["product_name"]
+    st.write(f"Product type: {name}")
